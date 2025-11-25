ed2c65ad78265b51c0e68e8e80aa90af47e39815
aqibmuhammad1239 <aqibmuhammad1239@gmail.com>
2025-11-25 04:50:42 -0800

    Add read_fifo.sv in fifo folderr

---
+++ rtl/fifo/read_fifo.sv
@@ -0,0 +1,15 @@
+module read_fifo(
+    parameter integer ADDR_WIDTH = 64,
+    parameter integer ID_WIDTH = 4,
+    parameter integer DEPTH = 8
+)(
+    input logic clk,
+    input logic rst_n,
+
+
+);
+
+
+
+
+endmodule
